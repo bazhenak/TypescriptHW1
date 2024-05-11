9a6a2105797e4a1beee30f170aa0c1c0a63fe8ee
bazhenak <bazhena.kutergina@gmail.com>
2024-05-11 16:41:39 +0300

    delete unused

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-fetch('https://jsonplaceholder.typicode.com/users').then((response) => {
-    console.log(response);
-    response.json().then((data) => {
-        console.log(data);
-    });
-});
-console.log('mama');
-//# sourceMappingURL=index.js.map
